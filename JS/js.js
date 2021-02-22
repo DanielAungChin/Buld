2ad4fec770f4d7efadf6ba402466dcc0a094b1ed
DanielChin <76833985+DanielAungChin@users.noreply.github.com>
2021-02-22 23:30:17 +0900

    first commit

---
+++ JS/js.js
@@ -0,0 +1,9 @@
+let img=document.getElementById("offimage");
+    
+    function On(){
+        img.src="images/ONBulb.jpg";
+    }
+
+    function Off(){
+        img.src="images/OFFBulb.jpg";
+    }
